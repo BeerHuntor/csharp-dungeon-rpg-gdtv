356ce9bb0e520f382e96e0261eebcc75a63d946b
BeerHuntor <rt001a9138@gmail.com>
2024-05-29 16:57:25 +0100

    working on mouse detection code for shits and giggles, trying to get a drag and drop system to work

--- scripts/player/Player.cs
+++ scripts/player/Player.cs
@@ -1,6 +1,16 @@
 using Godot;
 using System;
 
-public partial class player : CharacterBody3D {
-    
+public partial class Player : CharacterBody3D {
+
+    //Called everytime a physics frame is called. 
+    public override void _PhysicsProcess(double delta){
+    }
+
+    public override void _Input(InputEvent @event){
+        if (@event is InputEventMouseButton button){ 
+
+       
+        }
+    }
 }

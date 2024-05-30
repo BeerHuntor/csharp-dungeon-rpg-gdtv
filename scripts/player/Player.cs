db649e4dfc1a5d935669c83d371889aff7368e30
BeerHuntor <rt001a9138@gmail.com>
2024-05-30 02:27:52 +0100

    added blockbench models for tomato plants, i dont know why this project isnt even 3d.. i got carried away with modeling

--- scripts/player/Player.cs
+++ scripts/player/Player.cs
@@ -3,14 +3,14 @@ using System;
 
 public partial class Player : CharacterBody3D {
 
-    //Called everytime a physics frame is called. 
-    public override void _PhysicsProcess(double delta){
-    }
+	//Called everytime a physics frame is called. 
+	public override void _PhysicsProcess(double delta){
+	}
 
-    public override void _Input(InputEvent @event){
-        if (@event is InputEventMouseButton button){ 
+	public override void _Input(InputEvent @event){
+		if (@event is InputEventMouseButton button){ 
 
-       
-        }
-    }
+	   
+		}
+	}
 }

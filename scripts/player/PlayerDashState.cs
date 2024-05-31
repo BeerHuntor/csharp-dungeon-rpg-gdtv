e0757801fbe4859d0ae162db63035a5068d70289
BeerHuntor <rt001a9138@gmail.com>
2024-05-31 15:30:14 +0100

    Stopped and started the PhysicsProcess where needed.  Created 'HandleDashMovement()' which is responsible for applying the velocity based on the last known move direction.

--- scripts/player/PlayerDashState.cs
+++ scripts/player/PlayerDashState.cs
@@ -4,22 +4,46 @@ using System;
 public partial class PlayerDashState : Node {
 
     private Player playerNode;
+    [Export] private float dashSpeed = 2f; 
     [Export] private Timer dashTimer; 
 
     public override void _Ready() {
         playerNode = GetOwner<Player>();
         dashTimer.Timeout += HandleDashTimeoutEvent;
+        SetPhysicsProcess(false);
+    }
+
+    public override void _PhysicsProcess(double delta) {
+        playerNode.MoveAndSlide(); //Does the actual moving 
     }
     private void HandleDashTimeoutEvent() {
         playerNode.GetStateMachineNode().SwitchState<PlayerIdleState>();
+        playerNode.Velocity = Vector3.Zero;
+        SetPhysicsProcess(false);
     }
-    
+    /// <summary>
+    /// Handles the setting of the players last known movement direction and applies a velocity * the given dashspeed.  
+    /// </summary>
+    private void HandleDashMovement() {
+        playerNode.Velocity = playerNode.GetLastMoveDir();
+        //player is idle
+        if (!playerNode.IsMoving()) {
+            playerNode.Velocity = new Vector3(playerNode.GetPlayerSpriteNode().Scale.X, 0, 0);
+        }
+        playerNode.Velocity *= dashSpeed;
+    }
+    /// <summary>
+    /// Godots built in 'events' system.  Godot utlizies notifications similar to events for various things, we have hooked into this to serve our own purposes. 
+    /// </summary>
+    /// <param name="what"> (int) Notification number that is set from  the notification</param>
     public override void _Notification(int what) {
         base._Notification(what);
 
         if (what == GameConstants.SWITCH_ANIMATION_STATE) {
             playerNode.GetPlayerAnimationPlayer().Play(GameConstants.ANIMATION_PLAYER_DASH);
             dashTimer.Start();
+            HandleDashMovement();
+            SetPhysicsProcess(true);
         }
     }
 }

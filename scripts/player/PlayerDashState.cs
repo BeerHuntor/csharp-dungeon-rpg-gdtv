ee8f26ba1647db42a131c62ace0b2a6f0847bc8d
HuntorGames <rt001a9138@gmail.com>
2024-06-02 18:25:42 +0100

    Added 'stuff' .. bulk commit

--- scripts/player/PlayerDashState.cs
+++ scripts/player/PlayerDashState.cs
@@ -4,48 +4,45 @@ using System;
 
 public partial class PlayerDashState : PlayerStateBase {
 
-    [Export] private float dashSpeed = 2f; 
-    [Export] private Timer dashTimer;
+	[Export] private float dashSpeed = 2f; 
+	[Export] private Timer dashTimer;
 
-    private bool isDashing; 
+	private bool isDashing; 
 
-    public override void _Ready() {
-        base._Ready();
-        dashTimer.Timeout += HandleDashTimeoutEvent;
-    }
+	public override void _Ready() {
+		base._Ready();
+		dashTimer.Timeout += HandleDashTimeoutEvent;
+	}
 
-    public override void _PhysicsProcess(double delta) {
-        playerNode.MoveAndSlide(); //Does the actual moving 
-    }
-    private void HandleDashTimeoutEvent() {
-        playerNode.GetStateMachineNode().SwitchState<PlayerIdleState>();
-        playerNode.Velocity = Vector3.Zero;
-        isDashing = false; 
-    }
-    /// <summary>
-    /// Handles the setting of the players last known movement direction and applies a velocity * the given dash speed.  
-    /// </summary>
-    private void HandleDashMovement() {
-        if (isDashing){ return; }
-        
-        playerNode.Velocity = playerNode.GetLastMoveDir();
-        
-        // Is player Idle.
-        if (!playerNode.IsMoving()) {
-            playerNode.Velocity = new Vector3(playerNode.GetPlayerSpriteNode().Scale.X, 0, 0);
-            return;
-        }
+	public override void _PhysicsProcess(double delta) {
+		playerNode.MoveAndSlide(); //Does the actual moving 
+	}
+	private void HandleDashTimeoutEvent() {
+		playerNode.GetStateMachineNode().SwitchState<PlayerIdleState>();
+		playerNode.Velocity = Vector3.Zero;
+		isDashing = false; 
+	}
+	/// <summary>
+	/// Handles the setting of the players last known movement direction and applies a velocity * the given dash speed.  
+	/// </summary>
+	private void HandleDashMovement() {
+		playerNode.Velocity = playerNode.GetLastMoveDir();
+		
+		// Is player Idle.
+		if (!playerNode.IsMoving()) {
+			playerNode.Velocity = new Vector3(playerNode.GetPlayerSpriteNode().Scale.X, 0, 0) * dashSpeed;
+			GD.Print(playerNode.Velocity);
+			return;
+		}
 
-        playerNode.Velocity = (new Vector3(playerNode.Velocity.X, 0, playerNode.Velocity.Y) * dashSpeed);
-    }
-    
-    protected override void EnterState() {
-        base.EnterState();
-        
-        playerNode.GetPlayerAnimationPlayer().Play(GameConstants.ANIMATION_PLAYER_DASH);
-        isDashing = true; 
-        dashTimer.Start();
-        HandleDashMovement();
-    }
+		playerNode.Velocity = (new Vector3(playerNode.Velocity.X, 0, playerNode.Velocity.Y) * dashSpeed);
+	}
+	
+	protected override void EnterState() {
+		base.EnterState();
+		playerNode.GetPlayerAnimationPlayer().Play(GameConstants.ANIMATION_PLAYER_DASH);
+		dashTimer.Start();
+		HandleDashMovement();
+	}
 }
 
--- scripts/player/PlayerRunState.cs
+++ scripts/player/PlayerRunState.cs
@@ -4,32 +4,32 @@ using System;
 
 public partial class PlayerRunState : PlayerStateBase {
 
-    public override void _PhysicsProcess(double delta) {
-        if (!playerNode.IsMoving()) {
-            playerNode.GetStateMachineNode().SwitchState<PlayerIdleState>();
-        }
-        HandleMovement();
-    }
-    
-    ///<summary>
-    /// Handles the player movement and is responsible for moving the player. 
-    ///</summary>
-    private void HandleMovement() {
-        playerNode.Velocity = new Vector3(playerNode.GetInputVectorNormalized().X, 0, playerNode.GetInputVectorNormalized().Y);
+	public override void _PhysicsProcess(double delta) {
+		if (!playerNode.IsMoving()) {
+			playerNode.GetStateMachineNode().SwitchState<PlayerIdleState>();
+		}
+		HandleMovement();
+	}
+	
+	///<summary>
+	/// Handles the player movement and is responsible for moving the player. 
+	///</summary>
+	private void HandleMovement() {
+		playerNode.Velocity = new Vector3(playerNode.GetInputVectorNormalized().X, 0, playerNode.GetInputVectorNormalized().Y);
 
-        playerNode.Velocity *= playerNode.GetMovementSpeed();
-        playerNode.MoveAndSlide();
-    }
+		playerNode.Velocity *= playerNode.GetMovementSpeed();
+		playerNode.MoveAndSlide();
+	}
 
-    public override void _Input(InputEvent @event) {
-        if (Input.IsActionJustPressed(GameConstants.INPUT_DASH)) {
-            playerNode.GetStateMachineNode().SwitchState<PlayerDashState>();
-        }
-    }
+	public override void _Input(InputEvent @event) {
+		if (Input.IsActionJustPressed(GameConstants.INPUT_DASH)) {
+			playerNode.GetStateMachineNode().SwitchState<PlayerDashState>();
+		}
+	}
 
-    protected override void EnterState() {
-        base.EnterState();
-        
-        playerNode.GetPlayerAnimationPlayer().Play(GameConstants.ANIMATION_PLAYER_RUN);
-    }
+	protected override void EnterState() {
+		base.EnterState();
+		
+		playerNode.GetPlayerAnimationPlayer().Play(GameConstants.ANIMATION_PLAYER_RUN);
+	}
 }

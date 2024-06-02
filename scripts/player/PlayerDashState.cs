5ef2e5d63bed77b461e2758c74ae0f70a1b03783
HuntorGames <rt001a9138@gmail.com>
2024-06-02 16:48:22 +0100

    Added conditional check against isDashing bool in an attempt to stop the player constantly dashing.  Also fixed bug where the dash dependant on which key combination you pressed, allowed you to dash on the Y axis.

--- scripts/player/PlayerDashState.cs
+++ scripts/player/PlayerDashState.cs
@@ -5,7 +5,9 @@ using System;
 public partial class PlayerDashState : PlayerStateBase {
 
     [Export] private float dashSpeed = 2f; 
-    [Export] private Timer dashTimer; 
+    [Export] private Timer dashTimer;
+
+    private bool isDashing; 
 
     public override void _Ready() {
         base._Ready();
@@ -18,25 +20,30 @@ public partial class PlayerDashState : PlayerStateBase {
     private void HandleDashTimeoutEvent() {
         playerNode.GetStateMachineNode().SwitchState<PlayerIdleState>();
         playerNode.Velocity = Vector3.Zero;
+        isDashing = false; 
     }
     /// <summary>
     /// Handles the setting of the players last known movement direction and applies a velocity * the given dash speed.  
     /// </summary>
     private void HandleDashMovement() {
+        if (isDashing){ return; }
+        
         playerNode.Velocity = playerNode.GetLastMoveDir();
         
         // Is player Idle.
         if (!playerNode.IsMoving()) {
             playerNode.Velocity = new Vector3(playerNode.GetPlayerSpriteNode().Scale.X, 0, 0);
+            return;
         }
-        
-        playerNode.Velocity *= dashSpeed;
+
+        playerNode.Velocity = (new Vector3(playerNode.Velocity.X, 0, playerNode.Velocity.Y) * dashSpeed);
     }
     
     protected override void EnterState() {
         base.EnterState();
         
         playerNode.GetPlayerAnimationPlayer().Play(GameConstants.ANIMATION_PLAYER_DASH);
+        isDashing = true; 
         dashTimer.Start();
         HandleDashMovement();
     }
